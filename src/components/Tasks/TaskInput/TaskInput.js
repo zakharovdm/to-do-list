bd79b58809cac1195a7b1634a417a9592f780567
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-09-14 22:48:23 +0300

    feat: add a highlighting of the incoming message when an empty value is sent

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -5,21 +5,32 @@ import "./TaskInput.css";
 
 const TaskInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const taskInputChangeHandler = (evt) => {
     setEnteredValue(evt.target.value);
+    if (evt.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = (evt) => {
     evt.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddTask(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Task</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <input
+          type="text"
+          onChange={taskInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add task</Button>
     </form>
