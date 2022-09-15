53ed8e40b506a93f217a6c7e551092d0e48a7ee1
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-09-15 22:42:16 +0300

    refactor: styled components Button and TaskInput with styled component

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,7 +1,35 @@
 import { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
+import styled from "styled-components";
+//import "./TaskInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "salmon" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const TaskInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -25,13 +53,10 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      <FormControl invalid={!isValid}>
         <label>Task</label>
-        <input
-          type="text"
-          onChange={taskInputChangeHandler}
-        />
-      </div>
+        <input type="text" onChange={taskInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add task</Button>
     </form>
   );
